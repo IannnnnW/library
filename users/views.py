643887dd5e25a1e8c199dc6f77d73a04e730db94
victor-nsengiyumva <jerrysyre@gmail.com>
2022-06-29 16:20:28 +0300

    login2.0

--- users/views.py
+++ users/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import auth
 
 # Create your views here.
 
 def login(request):
-    return render(request,'books/login.html')
+    if request.method == 'POST':
+        Username = request.POST.get('username')
+        Password = request.POST['password']
+
+        user = auth.authenticate(username=Username,password=Password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+    else:
+        return render(request,'books/login.html')
 
 
 def register(request):
