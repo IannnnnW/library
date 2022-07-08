b1859804d15bde19191d470645a5d755bca5c537
victor-nsengiyumva <jerrysyre@gmail.com>
2022-07-08 16:00:32 +0300

    borrow

--- books/views.py
+++ books/views.py
@@ -52,11 +52,13 @@ def confirm_borrow(request,id):
     borrower = Borrower(first_name=request.user.first_name,last_name=request.user.last_name,book_name=book.title)
     borrower.save()
  
-    requested_book = RequestedBook(book_name = book.title,issued_date = datetime.now(),return_date=get_return_date() ,pickup_time = book_time_limit(),borrower=request.user)
+    requested_book = RequestedBook(book_name = book.title ,pickup_time = book_time_limit(),borrower=request.user)
     requested_book.save()
+    book.status = False
+    book.save()
 
     
-    return redirect('books:index')
+    return redirect('books:home')
 
 
 
