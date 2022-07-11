253ff409856c6184b81dd68df39d0ebbe9d14ab7
nsenji <jerrysyre@gmail>
2022-07-11 15:57:08 +0300

    Ready for deployment to heroku.

--- library_system/settings.py
+++ library_system/settings.py
@@ -88,29 +88,13 @@ WSGI_APPLICATION = 'library_system.wsgi.application'
 
 DATABASES = {
     'default': {
-<<<<<<< HEAD
         'ENGINE':'django.db.backends.postgresql',
         'NAME': 'victor6',
-=======
-        'ENGINE': 'django.db.backends.postgresql',
-<<<<<<< HEAD
-        'NAME': 'vinx2db',
->>>>>>> dcb92ba7be352922f7c9601b9920ece5d3b8ccd5
         'USER': 'postgres',
         'PASSWORD':'ilikedj48',
         'HOST': 'localhost',
         'PORT': '5432',
 
-<<<<<<< HEAD
-=======
-=======
-        'NAME': 'Library',
-        'USER': 'postgres',
-        'PASSWORD':'12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
->>>>>>> e6421a2edc5942384c99e89253986c3c57a23d71
->>>>>>> dcb92ba7be352922f7c9601b9920ece5d3b8ccd5
     }
 }
 
@@ -167,3 +151,8 @@ CRISPY_TEMPLATE_PACK="bootstrap4"
 #My Settings
 LOGIN_URL = 'users:login'
 LOGOUT_REDIRECT_URL = "books:index"
+
+
+# Heroku settings.
+import django_heroku
+django_heroku.settings(locals())
